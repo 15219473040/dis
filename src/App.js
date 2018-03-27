b3926e368ce774d81f9239f4b5e8957e5b6bcf38
15219473040 <2362999724@qq.com>
2018-03-27 18:32:14 +0800

    context

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 import CommentApp from "./components/common/CommentApp"
 import HeigherOrderComponent from "./components/step3/HeigherOrderComponent"
-
+import Context from "./components/step3/context/Context"
 
 
 import {
@@ -17,13 +17,15 @@ export default class App extends Component {
         
         return <Router>
             <div>
-                <Link to='/Comment'>CommentApp</Link>
-                <Link to='/HeigherOrderComponents'>HeigherOrderComponents</Link>
+                <Link to='/Comment' className="nav_item">CommentApp</Link>
+                <Link to='/HeigherOrderComponents' className="nav_item">HeigherOrderComponents</Link>
+                <Link to='/Context' className="nav_item">Context</Link>
 
 
                 <Route path='/' exact component={CommentApp} />
                 <Route path='/Comment'  component={CommentApp} />
                 <Route path='/HeigherOrderComponents' component={HeigherOrderComponent} />
+                <Route path='/Context' component={Context} />
 
             </div>
         </Router>
